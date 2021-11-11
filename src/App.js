dee6b2c7cacac30713ca73e14200f8ea2cccf093
jSilver <julesrsilver@gmail.com>
2021-11-11 12:50:36 -0800

    react-bs added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import ShowCards from './Components/ShowCards';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from 'react-bootstrap';
+
+// test Button added along with bootstrap core styling, feel free to remove - jules
 
 function App() {
   return (
     <div className="App">
       <ShowCards />
+      <Button variant="dark">Dark Test Button</Button>{' '}
     </div>
   );
 }
