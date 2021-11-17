0df1de52ab56eb64f625c741aece9a4aeea19b28
jSilver <julesrsilver@gmail.com>
2021-11-16 21:20:20 -0800

    added static page content

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -3,7 +3,24 @@ import React from 'react'
 function About() {
   return (
     <div>
-      
+      <h2>Tarot History</h2>
+      <p>
+        Tarot has been around for centuries across the world. The Western Tarot system has a variety of uses ranging from card games, divination to psychology and self-discovery. The deck consists of 78 cards and are divided into what is known as the Major Arcana and The Minor Arcana. These are shuffled together and laid out in a different patterns known as a "spread". The spread can be read and interpreted by a tarot practioner for another person, identified as the "querent". 
+      </p>
+      <p>
+        In recent years Tarot has shifted into the mainstream and is not just for the experts. It has served as a another means of self-discovery for those looking for another tool in this journey through life. 
+      </p>
+      <h2>About This Deck</h2>
+      <p>
+        The deck used is known as the Rider Waite Smith Deck. It was previously called the Rider Waite deck named after the instructor Andrew Edward Waite and the publisher the William Rider.
+      </p>
+      <p>
+        The artwork used in this deck is by the illustrator who up until recent years had not be widely recognized. The illustrator was none other than Pamela Colman Smith. 
+      </p>
+      <p>
+        Read more about <a href='https://www.shondaland.com/inspire/books/a21940524/demystifying-pamela-colman-smith/'>Pamela Colman Smith</a>.
+      </p>
+      <a href='/'>Return Home</a>
     </div>
   )
 }
--- src/Components/pages/Instructions.js
+++ src/Components/pages/Instructions.js
@@ -3,7 +3,39 @@ import React from 'react'
 function Instructions() {
   return (
     <div>
-      
+      <h2>Instructions</h2>
+      <p>
+        <i>This app is for entertainment purposes only. It is to showcase skills used to make an interactive app for those interested in learning more about tarot.</i>
+      </p>
+      <p>
+        Our Tarot Reading App is a fun introduction to tarot through a simple three card spread representing the past, present and future. 
+      </p>
+      <p>
+        The user will input a question and the app will shuffle a deck of cards and reveal a three card spread. 
+      </p>
+      <p>
+        The general meaning of the cards will be displayed and can be intrepeted by the user, who can then ask another question for themselves or a friend!
+      </p>
+      <h2>Reading the Cards</h2>
+      <p>
+        This app is using the Three Card Spread which can represent the Past, Present and Future. When presented with your cards, use the following as a reference for reading the spread as related to your question. 
+      </p>
+      <p>
+        <strong>Past:</strong>
+        <br/>
+        This represents any past influences, that can either propel you or block you. Lessons learned or lessons that need to be revisted. 
+      </p>
+      <p>
+        <strong>Present:</strong>
+        <br/>
+        Present: This represents you and environment. Where you are right now.  
+      </p>
+      <p>
+        <strong>Future:</strong>
+        <br/>
+        Outcome; the culmination of the energy of the first two cards. 
+      </p>
+      <a href='/'>Return Home</a>
     </div>
   )
 }
